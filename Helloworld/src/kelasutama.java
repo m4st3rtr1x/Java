fc4d24096db8184f34e9498400eb75e6767109de
richid <richid@192.168.100.141>
2020-03-26 00:22:12 +0700

    menambah message

---
+++ Helloworld/src/kelasutama.java
@@ -0,0 +1,18 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author richid
+ */
+public class kelasutama {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Selamat Belajar Pemrograman Visual Java");
+    }
+}
